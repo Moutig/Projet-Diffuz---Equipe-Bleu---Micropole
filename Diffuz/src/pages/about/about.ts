3f682d815cd395ee8da2a13d478bb8104c5fc2e5
Orele <lardeuxaurelie@gmail.com>
2017-11-19 00:11:01 +0100

    sauvegarde de minuit

--- Diffuz/src/pages/about/about.ts
+++ Diffuz/src/pages/about/about.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FilterPage } from '../filter/filter';
 import { DefiPage } from '../defi/defi';
+import { CreatedefiPage } from '../createdefi/createdefi';
 
 @Component({
   selector: 'page-about',
@@ -10,6 +11,7 @@ import { DefiPage } from '../defi/defi';
 export class AboutPage {
   filterPage = FilterPage;
   defiPage = DefiPage;
+  createdefiPage = CreatedefiPage;
   constructor(public navCtrl: NavController) {
 
   }
