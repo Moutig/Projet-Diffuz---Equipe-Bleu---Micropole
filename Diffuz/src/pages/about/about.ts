ce79ff6829a03ec74578ee4e522026474f1deacb
Orele <lardeuxaurelie@gmail.com>
2017-11-18 16:02:36 +0100

    sauvegarde full display

--- Diffuz/src/pages/about/about.ts
+++ Diffuz/src/pages/about/about.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FilterPage } from '../filter/filter';
+import { DefiPage } from '../defi/defi';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
+  filterPage = FilterPage;
+  defiPage = DefiPage;
   constructor(public navCtrl: NavController) {
 
   }
